f8b4b990e255477e61ba1c7432a0193113a8ea8c
Jason T. Greene <jason@stacksmash.com>
2010-05-11 15:46:04 -0500

    Add initial batch API

---
+++ src/main/java/org/jboss/msc/resolver/ServiceBatch.java
@@ -0,0 +1,61 @@
+package org.jboss.msc.resolver;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+
+/**
+ * An ordered set of service definitions that should be processed as one.
+ * 
+ * @author Jason T. Greene
+ */
+public final class ServiceBatch {
+
+    private LinkedHashMap<String, ServiceDefinition> definitions;
+    
+    /**
+     * Add a service definition to the batch. 
+     * 
+     * @param definition
+     * @return this batch
+     */
+    public ServiceBatch add(ServiceDefinition definition)
+    {
+        definitions.put(definition.getName(), definition);
+        
+        return this;
+    }
+    
+    /**
+     * Add a list of service definitions to the batch, in the order of the list.
+     * 
+     * @param definitions add a list of service definitions to the batch, in the order of the list
+     * @return this batch
+     */
+    public ServiceBatch add(ServiceDefinition... definitions)
+    {
+        for (ServiceDefinition d : definitions)
+            this.definitions.put(d.getName(), d);
+        
+        return this;
+    }
+    
+    /**
+     * Add a collection of service definitions to the batch, in the order of the collection (if ordered).
+     * 
+     * @param definitions add a list of service definitions to the batch, in the order of the list
+     * @return this batch
+     */
+    public ServiceBatch add(Collection<ServiceDefinition> definitions)
+    {
+        for (ServiceDefinition d : definitions)
+            this.definitions.put(d.getName(), d);
+        
+        return this;
+    }
+    
+    LinkedHashMap<String, ServiceDefinition> definitionMap()
+    {
+        return definitions;
+    }
+
+}
