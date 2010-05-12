6d52a0e6faa93045e8d28ad86636cf0cecf48e5e
Jason T. Greene <jason@stacksmash.com>
2010-05-12 15:19:34 -0500

    Fix circulatory checking

--- src/main/java/org/jboss/msc/registry/ServiceRegistry.java
+++ src/main/java/org/jboss/msc/registry/ServiceRegistry.java
@@ -124,7 +124,6 @@ public class ServiceRegistry {
         while (entry != null) {
             if (entry.isVisited())
                 throw new CircularDependencyException("Circular dependency discovered: " + entry.getServiceDefinition());
-            entry.setVisited(true);
             
             if (entry.builder == null)
                 entry.builder = serviceContainer.buildService(entry.getServiceDefinition().getService());
@@ -163,6 +162,7 @@ public class ServiceRegistry {
             
             // Unroll!
             entry.setProcessed(true);
+            entry.setVisited(true);
             entry = entry.prev;
         }
     }
