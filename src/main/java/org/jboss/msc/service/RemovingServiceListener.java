7da6b24468bd910d2f6882b4ff60a6f43f665143
David M. Lloyd <dmlloyd@flurg.com>
2010-05-11 22:43:13 -0500

    A "removing" service listener

---
+++ src/main/java/org/jboss/msc/service/RemovingServiceListener.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.msc.service;
+
+/**
+ * A service listener which removes the given service as soon as it is down, used to "kill" a service.
+ *
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
+ */
+public final class RemovingServiceListener extends AbstractServiceListener<Object> {
+
+    /** {@inheritDoc} */
+    public void serviceStarting(final ServiceController<? extends Object> serviceController) {
+        serviceController.setMode(ServiceController.Mode.NEVER);
+    }
+
+    /** {@inheritDoc} */
+    public void serviceStarted(final ServiceController<? extends Object> serviceController) {
+        serviceController.setMode(ServiceController.Mode.NEVER);
+    }
+
+    /** {@inheritDoc} */
+    public void serviceFailed(final ServiceController<? extends Object> serviceController, final StartException reason) {
+        serviceController.setMode(ServiceController.Mode.NEVER);
+    }
+
+    /** {@inheritDoc} */
+    public void serviceStopping(final ServiceController<? extends Object> serviceController) {
+        serviceController.setMode(ServiceController.Mode.NEVER);
+    }
+
+    /** {@inheritDoc} */
+    public void serviceStopped(final ServiceController<?> serviceController) {
+        serviceController.setMode(ServiceController.Mode.NEVER);
+        serviceController.remove();
+    }
+}
