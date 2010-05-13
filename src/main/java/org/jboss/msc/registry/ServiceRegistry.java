c04cd515aa81cdc02858aa139833f15f7076efb6
David M. Lloyd <dmlloyd@flurg.com>
2010-05-12 20:28:04 -0500

    Use interfaces, dammit! :-)

--- src/main/java/org/jboss/msc/registry/ServiceRegistry.java
+++ src/main/java/org/jboss/msc/registry/ServiceRegistry.java
@@ -19,174 +19,80 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.jboss.msc.registry;
 
-import org.jboss.msc.service.AbstractServiceListener;
-import org.jboss.msc.service.ServiceBuilder;
+import java.util.Collection;
 import org.jboss.msc.service.ServiceContainer;
-import org.jboss.msc.service.ServiceController;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
- * Service registry capable of installing batches of services and enforcing dependency order. 
- *
- * @author John Bailey
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
-public class ServiceRegistry {
-    private final ConcurrentMap<ServiceName, ServiceController<?>> registry = new ConcurrentHashMap<ServiceName, ServiceController<?>>();
+public interface ServiceRegistry {
 
-    private final ServiceContainer serviceContainer;
+    /**
+     * Get a new batch builder, which is used to resolve and install described services.
+     *
+     * @return the new batch builder
+     */
+    BatchBuilder batchBuilder();
 
-    public ServiceRegistry(ServiceContainer serviceContainer) {
-        this.serviceContainer = serviceContainer;
-    }
+    /**
+     * A batch builder for installing service definitions in a single action.
+     */
+    interface BatchBuilder {
 
-    public ServiceBatch create() {
-        return new ServiceBatch(this);
+        /**
+         * Install all the defined services into the container.
+         *
+         * @throws ServiceRegistryException
+         */
+        void install() throws ServiceRegistryException;
+
+        /**
+         * Add a service definition to the batch.
+         *
+         * @param definition
+         * @return this batch
+         */
+        BatchBuilderImpl add(ServiceDefinition definition);
+
+        /**
+         * Add a list of service batchEntries to the batch, in the order of the list.
+         *
+         * @param definitions add a list of service batchEntries to the batch, in the
+         *        order of the list
+         * @return this batch
+         */
+        BatchBuilderImpl add(ServiceDefinition<?>... definitions);
+
+        /**
+         * Add a collection of service batchEntries to the batch, in the order of the
+         * collection (if ordered).
+         *
+         * @param definitions add a list of service batchEntries to the batch, in the
+         *        order of the list
+         * @return this batch
+         */
+        BatchBuilderImpl add(Collection<ServiceDefinition<?>> definitions);
     }
 
     /**
-     * Install a collection of service definitions into the registry.  Will install the services
-     * in dependency order.
-     *
-     * @param serviceBatch The service batch to install
-     * @throws ServiceRegistryException If any problems occur resolving the dependencies or adding to the registry.
+     * The factory used to create instances.
      */
-    void install(final ServiceBatch serviceBatch) throws ServiceRegistryException {
-        try {
-            resolve(serviceBatch.getBatchEntries());
-        } catch (ResolutionException e) {
-            throw new ServiceRegistryException("Failed to resolve dependencies", e);
-        }
-    }
+    class Factory {
 
-    /**
-     * Recursive depth-first resolution
-     *
-     * @param services The list of items to be resolved
-     * @throws ResolutionException if any problem occur during resolution
-     */
-    private void resolve(final Map<ServiceName, ServiceBatch.BatchEntry> services) throws ServiceRegistryException {
-        for (ServiceBatch.BatchEntry batchEntry : services.values()) {
-            if(!batchEntry.processed)
-                iterResolve(batchEntry, services);
-        }
-    }
-
-    private ServiceController<?> resolve(final ServiceBatch.BatchEntry entry, final Map<ServiceName, ServiceBatch.BatchEntry> services) throws ServiceRegistryException {
-        final ServiceDefinition<?> serviceDefinition = entry.serviceDefinition;
-        entry.processed = true;
-        final ServiceName name = serviceDefinition.getName();
-        if (entry.visited)
-            throw new CircularDependencyException("Circular dependency discovered: " + serviceDefinition);
-
-        entry.visited = true;
-
-        try {
-            final ConcurrentMap<ServiceName, ServiceController<?>> registry = this.registry;
-            final ServiceBuilder<?> builder = serviceContainer.buildService(serviceDefinition.getService());
-
-            for (String dependency : serviceDefinition.getDependenciesDirect()) {
-                final ServiceName dependencyName = ServiceName.create(dependency);
-
-                ServiceController<?> dependencyController = registry.get(dependencyName);
-                if (dependencyController == null) {
-                    final ServiceBatch.BatchEntry dependencyEntry = services.get(dependencyName);
-                    if (dependencyEntry == null)
-                        throw new MissingDependencyException("Missing dependency: " + name + " depends on " + dependencyName + " which can not be found");
-                    dependencyController = resolve(dependencyEntry, services);
-                }
-                builder.addDependency(dependencyController);
-            }
-
-            // We are resolved.  Lets install
-            builder.addListener(new ServiceUnregisterListener(name));
-
-            final ServiceController<?> serviceController = builder.create();
-            if (registry.putIfAbsent(name, serviceController) != null) {
-                throw new DuplicateServiceException("Duplicate service name provided: " + name);
-            }
-            return serviceController;
-        } finally {
-            entry.visited = false;
-        }
-    }
-    
-    public void iterResolve(ServiceBatch.BatchEntry entry, final Map<ServiceName, ServiceBatch.BatchEntry> services)  throws ServiceRegistryException
-    {
-        outer:
-        while (entry != null) {
-            final ServiceDefinition<?> serviceDefinition = entry.serviceDefinition;
-            
-            if (entry.builder == null)
-                entry.builder = serviceContainer.buildService(serviceDefinition.getService());
-            
-            final ServiceBuilder<?> builder = entry.builder;
-            final String[] deps = serviceDefinition.getDependenciesDirect();
-            final ServiceName name = serviceDefinition.getName();
-            
-            while (entry.i < deps.length)
-            {
-                final ServiceName dependencyName = ServiceName.create(deps[entry.i]);
-        
-                ServiceController<?> dependencyController = registry.get(dependencyName);     
-                if (dependencyController == null){
-                    final ServiceBatch.BatchEntry dependencyEntry = services.get(dependencyName);
-                    if (dependencyEntry == null)
-                        throw new MissingDependencyException("Missing dependency: " + name + " depends on " + dependencyName + " which can not be found");
-                 
-                    // Backup the last position, so that we can unroll
-                    assert dependencyEntry.prev == null;
-                    dependencyEntry.prev = entry;
-
-                    entry.visited = true;
-                    entry = dependencyEntry;
-
-                    if (entry.visited)
-                        throw new CircularDependencyException("Circular dependency discovered: " + serviceDefinition);
-
-                    continue outer;
-                }
-                
-                // Either the dep already exists, or we are unrolling and just created it
-                builder.addDependency(dependencyController);
-                entry.i++;
-            }
-            
-            // We are resolved.  Lets install
-            builder.addListener(new ServiceUnregisterListener(name));
-
-            final ServiceController<?> serviceController = builder.create();
-            if (registry.putIfAbsent(name, serviceController) != null) {
-                throw new DuplicateServiceException("Duplicate service name provided: " + name);
-            }
-            
-            // Cleanup
-            entry.builder = null;
-            ServiceBatch.BatchEntry prev = entry.prev;
-            entry.prev = null;
-            
-            // Unroll!
-            entry.processed = true;
-            entry.visited = false;
-            entry = prev;
-        }
-    }
-
-    private class ServiceUnregisterListener extends AbstractServiceListener<Object> {
-        private final ServiceName serviceName;
-
-        private ServiceUnregisterListener(ServiceName serviceName) {
-            this.serviceName = serviceName;
+        private Factory() {
         }
 
-        @Override
-        public void serviceRemoved(ServiceController serviceController) {
-            if(!registry.remove(serviceName, serviceController))
-                throw new RuntimeException("Removed service [" + serviceName + "] was not unregistered");
+        /**
+         * Create a new service registry instance.
+         *
+         * @param container the associated container
+         * @return the new registry
+         */
+        public static ServiceRegistry create(ServiceContainer container) {
+            return new ServiceRegistryImpl(container);
         }
     }
 }
