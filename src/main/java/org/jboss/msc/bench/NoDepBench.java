ad54b65206a45beb013f3ebf3d8c69131091c209
David M. Lloyd <dmlloyd@flurg.com>
2010-05-13 19:55:49 -0500

    Mismerge

--- src/main/java/org/jboss/msc/bench/NoDepBench.java
+++ src/main/java/org/jboss/msc/bench/NoDepBench.java
@@ -1,6 +1,7 @@
 package org.jboss.msc.bench;
 
 import org.jboss.msc.registry.ServiceDefinition;
+import org.jboss.msc.registry.ServiceRegistrationBatchBuilder;
 import org.jboss.msc.registry.ServiceRegistry;
 import org.jboss.msc.service.Service;
 import org.jboss.msc.service.ServiceContainer;
@@ -11,7 +12,7 @@ public class NoDepBench {
     public static void main(String[] args) throws Exception {
         final int totalServiceDefinitions = Integer.parseInt(args[0]);
 
-        ServiceRegistry.BatchBuilder batch = ServiceRegistry.Factory.create(ServiceContainer.Factory.create()).batchBuilder();
+        ServiceRegistrationBatchBuilder batch = ServiceRegistry.Factory.create(ServiceContainer.Factory.create()).batchBuilder();
 
         long start = System.nanoTime();
         
