3ac5fb972f2b2360020a952d6b7f82b5936ecd37
John Bailey <baileyje@gmail.com>
2010-05-17 14:33:28 -0500

    Add a benchmark for busy starts

---
+++ src/main/java/org/jboss/msc/bench/NoDepsBusyStartBench.java
@@ -0,0 +1,66 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.msc.bench;
+
+import org.jboss.msc.registry.ServiceDefinition;
+import org.jboss.msc.registry.ServiceRegistrationBatchBuilder;
+import org.jboss.msc.registry.ServiceRegistry;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.ServiceContainer;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.TimingServiceListener;
+import org.jboss.msc.value.Value;
+
+import java.util.concurrent.CountDownLatch;
+
+public class NoDepsBusyStartBench {
+
+    public static void main(String[] args) throws Exception {
+        final int totalServiceDefinitions = Integer.parseInt(args[0]);
+
+        final ServiceContainer container = ServiceContainer.Factory.create();
+        final ServiceRegistry registry = ServiceRegistry.Factory.create(container);
+        ServiceRegistrationBatchBuilder batch = registry.batchBuilder();
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        final TimingServiceListener listener = new TimingServiceListener(new TimingServiceListener.FinishListener() {
+            public void done(final TimingServiceListener timingServiceListener) {
+                latch.countDown();
+            }
+        });
+
+        for (int i = 0; i < totalServiceDefinitions; i++) {
+            final BusyService service = new BusyService();
+            final ServiceDefinition.Builder<BusyService> builder = ServiceDefinition.build(ServiceName.of(("test" + i).intern()), (Value<? extends Service<BusyService>>)service)
+                    .addListener(listener);
+
+            batch.add(builder.create());
+        }
+
+        batch.install();
+        listener.finishBatch();
+        latch.await();
+        System.out.println(totalServiceDefinitions + " : " + listener.getElapsedTime() / 1000.0);
+        container.shutdown();
+    }
+}
