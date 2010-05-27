6ba570d58180de4a1b28a396e8ab9dc430af1348
David M. Lloyd <dmlloyd@flurg.com>
2010-05-27 17:38:45 -0500

    Integration with javax.inject

--- src/main/java/org/jboss/msc/value/ProviderValue.java
+++ src/main/java/org/jboss/msc/value/ProviderValue.java
@@ -1,8 +1,28 @@
 package org.jboss.msc.value;
 
+import javax.inject.Provider;
+
 /**
+ * A value which creates an instance via a {@link Provider}.
+ *
+ * @param <T> the value type
+ *
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
-public final class ProviderValue {
+public final class ProviderValue<T> implements Value<T> {
+    private final Provider<T> provider;
 
+    /**
+     * Create a new instance.
+     *
+     * @param provider the provider to use
+     */
+    public ProviderValue(final Provider<T> provider) {
+        this.provider = provider;
+    }
+
+    /** {@inheritDoc} */
+    public T getValue() throws IllegalStateException {
+        return provider.get();
+    }
 }
