545d313c1d13baa461527c9fa097789925156e2a
David M. Lloyd <david.lloyd@redhat.com>
2011-02-13 14:08:33 -0600

    Track the actual controllers added in the batch, not just the names

--- src/main/java/org/jboss/msc/service/BatchServiceTargetImpl.java
+++ src/main/java/org/jboss/msc/service/BatchServiceTargetImpl.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright 2011, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -24,124 +24,104 @@ package org.jboss.msc.service;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 
 import org.jboss.msc.service.ServiceController.Mode;
+import org.jboss.msc.value.ImmediateValue;
 import org.jboss.msc.value.Value;
 
 /**
  * {@link BatchServiceTarget} implementation.
  * 
  * @author <a href="mailto:flavia.rainone@jboss.com">Flavia Rainone</a>
+ * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
-class BatchServiceTargetImpl implements BatchServiceTarget {
+class BatchServiceTargetImpl extends DelegatingServiceTarget implements BatchServiceTarget {
 
     private final ServiceTarget serviceTarget;
     private final ServiceRegistry serviceRegistry;
-    private final Collection<ServiceName> addedServiceNames;
+    private final Collection<ServiceController<?>> addedServiceControllers;
 
     BatchServiceTargetImpl(ServiceTarget serviceTarget, ServiceRegistry serviceRegistry) {
+        super(serviceTarget.subTarget());
         this.serviceTarget = serviceTarget.subTarget();
         this.serviceRegistry = serviceRegistry;
-        this.addedServiceNames = new HashSet<ServiceName>();
+        addedServiceControllers = new HashSet<ServiceController<?>>();
     }
 
     @Override
     public void removeServices() {
-        synchronized(addedServiceNames) {
-            for (ServiceName name: addedServiceNames) {
-                ServiceController<?> serviceController = serviceRegistry.getService(name);
+        final Collection<ServiceController<?>> controllers = addedServiceControllers;
+        synchronized(controllers) {
+            for (ServiceController<?> serviceController : controllers) {
                 if (serviceController != null) {
                     serviceController.setMode(Mode.REMOVE);
                 }
             }
-            addedServiceNames.clear();
+            controllers.clear();
         }
     }
 
     @Override
     public <T> ServiceBuilder<T> addServiceValue(ServiceName name, Value<? extends Service<T>> value) {
-        ServiceBuilder<T> serviceBuilder = serviceTarget.addServiceValue(name, value);
-        synchronized (addedServiceNames) {
-            addedServiceNames.add(name);
-        }
-        return serviceBuilder;
+        return new DelegatingServiceBuilder<T>(super.addServiceValue(name, value)) {
+            public ServiceController<T> install() throws ServiceRegistryException {
+                ServiceController<T> installed = super.install();
+                final Collection<ServiceController<?>> controllers = addedServiceControllers;
+                synchronized (controllers) {
+                    controllers.add(installed);
+                }
+                return installed;
+            }
+        };
     }
 
     @Override
     public <T> ServiceBuilder<T> addService(ServiceName name, Service<T> service) {
-        ServiceBuilder<T> serviceBuilder = serviceTarget.addService(name, service);
-        synchronized (addedServiceNames) {
-            addedServiceNames.add(name);
-        }
-        return serviceBuilder;
-    }
-
-    @Override
-    public BatchServiceTarget addListener(ServiceListener<Object> listener) {
-        serviceTarget.addListener(listener);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget addListener(ServiceListener<Object>... listeners) {
-        serviceTarget.addListener(listeners);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget addListener(Collection<ServiceListener<Object>> listeners) {
-        serviceTarget.addListener(listeners);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget removeListener(ServiceListener<Object> listener) {
-        serviceTarget.removeListener(listener);
-        return this;
-    }
-
-    @Override
-    public Set<ServiceListener<Object>> getListeners() {
-        return serviceTarget.getListeners();
-    }
-
-    @Override
-    public BatchServiceTarget addDependency(ServiceName dependency) {
-        serviceTarget.addDependency(dependency);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget addDependency(ServiceName... dependencies) {
-        serviceTarget.addDependency(dependencies);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget addDependency(Collection<ServiceName> dependencies) {
-        serviceTarget.addDependency(dependencies);
-        return this;
-    }
-
-    @Override
-    public BatchServiceTarget removeDependency(ServiceName dependency) {
-        serviceTarget.removeDependency(dependency);
-        return this;
-    }
-
-    @Override
-    public Set<ServiceName> getDependencies() {
-        return serviceTarget.getDependencies();
-    }
-
-    @Override
-    public ServiceTarget subTarget() {
-        return serviceTarget.subTarget();
+        return addServiceValue(name, new ImmediateValue<Service<T>>(service));
     }
 
     @Override
     public BatchServiceTarget batchTarget() {
         return new BatchServiceTargetImpl(serviceTarget, serviceRegistry);
+    }
+
+    public BatchServiceTarget addListener(final ServiceListener<Object> listener) {
+        super.addListener(listener);
+        return this;
+    }
+
+    public BatchServiceTarget addListener(final ServiceListener<Object>... listeners) {
+        super.addListener(listeners);
+        return this;
+    }
+
+    public BatchServiceTarget addListener(final Collection<ServiceListener<Object>> listeners) {
+        super.addListener(listeners);
+        return this;
+    }
+
+    public BatchServiceTarget removeListener(final ServiceListener<Object> listener) {
+        super.removeListener(listener);
+        return this;
+    }
+
+    public BatchServiceTarget addDependency(final ServiceName dependency) {
+        super.addDependency(dependency);
+        return this;
+    }
+
+    public BatchServiceTarget addDependency(final ServiceName... dependencies) {
+        super.addDependency(dependencies);
+        return this;
+    }
+
+    public BatchServiceTarget addDependency(final Collection<ServiceName> dependencies) {
+        super.addDependency(dependencies);
+        return this;
+    }
+
+    public BatchServiceTarget removeDependency(final ServiceName dependency) {
+        super.removeDependency(dependency);
+        return this;
     }
 }
