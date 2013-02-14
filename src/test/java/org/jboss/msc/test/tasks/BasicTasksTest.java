e05c3d350509098c9f8b00d9e9aedc3b86bb0483
David M. Lloyd <david.lloyd@redhat.com>
2013-02-14 11:35:23 -0600

    Basic rollback test

--- src/test/java/org/jboss/msc/test/tasks/BasicTasksTest.java
+++ src/test/java/org/jboss/msc/test/tasks/BasicTasksTest.java
@@ -25,6 +25,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.jboss.msc.txn.Executable;
 import org.jboss.msc.txn.ExecuteContext;
+import org.jboss.msc.txn.Revertible;
+import org.jboss.msc.txn.RollbackContext;
 import org.jboss.msc.txn.TaskBuilder;
 import org.jboss.msc.txn.TaskController;
 import org.jboss.msc.txn.Transaction;
@@ -39,7 +41,7 @@ import static org.junit.Assert.assertTrue;
 public class BasicTasksTest {
 
     @Test
-    public void testTaskCreate() throws InterruptedException {
+    public void testSimpleExecute() throws InterruptedException {
         final ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 0L, TimeUnit.DAYS, new LinkedBlockingQueue<Runnable>());
         final Transaction transaction = Transaction.create(executor);
         final AtomicBoolean ran = new AtomicBoolean();
@@ -62,6 +64,43 @@ public class BasicTasksTest {
         });
         latch.await();
         assertTrue(ran.get());
+        controller.getResult();
+        assertTrue(executor.shutdownNow().isEmpty());
+    }
+
+    @Test
+    public void testSimpleRollback() throws InterruptedException {
+        final ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 0L, TimeUnit.DAYS, new LinkedBlockingQueue<Runnable>());
+        final Transaction transaction = Transaction.create(executor);
+        final AtomicBoolean ran = new AtomicBoolean();
+        final AtomicBoolean reverted = new AtomicBoolean();
+        class Task implements Executable<Object>, Revertible {
+
+            public void execute(final ExecuteContext<Object> context) {
+                ran.set(true);
+                context.complete();
+            }
+
+            public void rollback(final RollbackContext context) {
+                reverted.set(true);
+                context.complete();
+            }
+        }
+        final TaskBuilder<Object> taskBuilder = transaction.newTask(new Task());
+        final TaskController<Object> controller = taskBuilder.release();
+        final CountDownLatch latch = new CountDownLatch(1);
+        transaction.prepare(new Listener<Transaction>() {
+            public void handleEvent(final Transaction subject) {
+                subject.rollback(new Listener<Transaction>() {
+                    public void handleEvent(final Transaction subject) {
+                        latch.countDown();
+                    }
+                });
+            }
+        });
+        latch.await();
+        assertTrue(ran.get());
+        assertTrue(reverted.get());
         controller.getResult();
         assertTrue(executor.shutdownNow().isEmpty());
     }
