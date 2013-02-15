61f0e0c5c8ecffe30a0a1a3b81d6a4678083d235
Richard Opalka <opalka.richard@gmail.com>
2013-02-15 13:52:09 +0100

    fixing test issue - validation request is not propagated to children tasks since we don't keep references to them

--- src/main/java/org/jboss/msc/txn/TaskControllerImpl.java
+++ src/main/java/org/jboss/msc/txn/TaskControllerImpl.java
@@ -879,6 +879,7 @@ final class TaskControllerImpl<T> extends TaskController<T> implements TaskParen
         assert ! holdsLock(this);
         int state;
         synchronized (this) {
+            children.add(child);
             state = this.state;
             if (stateIsIn(state, STATE_EXECUTE_WAIT, STATE_EXECUTE)) {
                 unfinishedChildren++;
