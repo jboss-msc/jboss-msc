fc440eb1c4b5964a06bb6bf4c5be0d9de7385b8e
Richard Opalka <opalka.richard@gmail.com>
2013-05-20 14:39:15 +0200

    proper synchronization to prevent ConcurrentModificationException during traversal

--- src/main/java/org/jboss/msc/service/ServiceTargetImpl.java
+++ src/main/java/org/jboss/msc/service/ServiceTargetImpl.java
@@ -224,9 +224,15 @@ class ServiceTargetImpl implements ServiceTarget {
      * @param serviceBuilder serviceBuilder which listeners and dependencies will be added to.
      */
     void apply(ServiceBuilderImpl<?> serviceBuilder) {
-        serviceBuilder.addMonitorsNoCheck(monitors);
-        serviceBuilder.addListenerNoCheck(listeners);
-        serviceBuilder.addDependenciesNoCheck(dependencies);
+        synchronized (monitors) {
+            serviceBuilder.addMonitorsNoCheck(monitors);
+        }
+        synchronized (listeners) {
+            serviceBuilder.addListenerNoCheck(listeners);
+        }
+        synchronized (dependencies) {
+            serviceBuilder.addDependenciesNoCheck(dependencies);
+        }
     }
 
     /**
