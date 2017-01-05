8d948ee0ea7230f5989cdc5fd1af2dedc175c472
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-01-05 13:47:49 +1100

    Remove unused field

--- src/main/java/org/jboss/msc/service/ServiceControllerImpl.java
+++ src/main/java/org/jboss/msc/service/ServiceControllerImpl.java
@@ -116,11 +116,6 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
      */
     private int demandedByCount;
     /**
-     * The number of dependencies which are not yet started.  This service cannot start until
-     * this count reaches zero.
-     */
-    private int unstartedDependencies;
-    /**
      * Count for dependencies that are trying to stop.  If this count is greater than zero then
      * dependents will be notified that a stop is necessary.
      */
@@ -201,7 +196,6 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
         }
         this.parent = parent;
         int depCount = dependencies.length;
-        unstartedDependencies = 0;
         stoppingDependencies = parent == null? depCount : depCount + 1;
         children = new IdentityHashSet<ServiceControllerImpl<?>>();
         immediateUnavailableDependencies = new IdentityHashSet<ServiceName>();
@@ -328,7 +322,7 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
      */
     private boolean shouldStart() {
         assert holdsLock(this);
-        return unstartedDependencies == 0 && (mode == Mode.ACTIVE || mode == Mode.PASSIVE || demandedByCount > 0 && (mode == Mode.ON_DEMAND || mode == Mode.LAZY));
+        return (mode == Mode.ACTIVE || mode == Mode.PASSIVE || demandedByCount > 0 && (mode == Mode.ON_DEMAND || mode == Mode.LAZY));
     }
 
     /**
@@ -338,7 +332,7 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
      */
     private boolean shouldStop() {
         assert holdsLock(this);
-        return unstartedDependencies > 0 || (mode == Mode.NEVER || mode == Mode.REMOVE || demandedByCount == 0 && mode == Mode.ON_DEMAND);
+        return (mode == Mode.NEVER || mode == Mode.REMOVE || demandedByCount == 0 && mode == Mode.ON_DEMAND);
     }
 
     /**
@@ -1422,7 +1416,6 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
             b.append("Service Object: ").append(serviceObjectString).append('\n');
             b.append("Service Object Class: ").append(serviceObjectClass).append('\n');
             b.append("Demanded By: ").append(demandedByCount).append('\n');
-            b.append("Unstarted Dependencies: ").append(unstartedDependencies).append('\n');
             b.append("Stopping Dependencies: ").append(stoppingDependencies).append('\n');
             b.append("Running Dependents: ").append(runningDependents).append('\n');
             b.append("Fail Count: ").append(failCount).append('\n');
