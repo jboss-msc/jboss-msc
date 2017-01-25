1067a504205dd03b0be08d91b72ec5ca5234791f
Richard Opálka <opalka.richard@gmail.com>
2017-01-25 16:26:49 +0100

    [MSC-166] Adding missing Listener notifications for the following transitions:  * START_REQUESTED -> START_INITIATING  * DOWN -> REMOVING

--- src/main/java/org/jboss/msc/service/ServiceControllerImpl.java
+++ src/main/java/org/jboss/msc/service/ServiceControllerImpl.java
@@ -547,6 +547,7 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
                     break;
                 }
                 case START_REQUESTED_to_START_INITIATING: {
+                    getListenerTasks(transition, tasks);
                     tasks.add(new DependentStartedTask());
                     break;
                 }
@@ -640,6 +641,7 @@ final class ServiceControllerImpl<S> implements ServiceController<S>, Dependent 
                     break;
                 }
                 case DOWN_to_REMOVING: {
+                    getListenerTasks(transition, tasks);
                     tasks.add(new ServiceUnavailableTask());
                     Dependent[][] dependents = getDependents();
                     // Clear all dependency uninstalled flags from dependents
