3f5cc857da31b901ba37e1b8197b643e1e5f4388
Richard Opálka <opalka.richard@gmail.com>
2019-09-11 20:04:24 +0200

    Refactoring tests covering issues to separate test package

--- src/test/java/org/jboss/msc/issues/MSC164TestCase.java
+++ src/test/java/org/jboss/msc/issues/MSC164TestCase.java
@@ -20,8 +20,9 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.jboss.msc.service;
+package org.jboss.msc.issues;
 
+import org.jboss.msc.service.*;
 import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
