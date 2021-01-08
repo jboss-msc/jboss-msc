2660c7d181294cb9e413b1ffea6bd3668ad59a81
Richard Opálka <opalka.richard@gmail.com>
2021-01-08 14:28:58 +0100

    Updating Copyright

--- src/test/java/org/jboss/msc/service/InstallDoesNotClearContextClassLoaderTestCase.java
+++ src/test/java/org/jboss/msc/service/InstallDoesNotClearContextClassLoaderTestCase.java
@@ -1,8 +1,8 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat Inc., and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -19,6 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.jboss.msc.service;
 
 import org.junit.Assert;
