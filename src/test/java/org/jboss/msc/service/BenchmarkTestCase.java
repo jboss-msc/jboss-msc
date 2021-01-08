810b6bb92b096654f27b32d032069d9000df3971
Richard Opálka <opalka.richard@gmail.com>
2021-01-08 14:28:58 +0100

    Updating Copyright

--- src/test/java/org/jboss/msc/service/BenchmarkTestCase.java
+++ src/test/java/org/jboss/msc/service/BenchmarkTestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2019, Red Hat, Inc., and individual contributors
+ * Copyright 2021, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -19,6 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
 package org.jboss.msc.service;
 
 import org.junit.Test;
--- src/test/java/org/jboss/msc/value/InjectedSetValueTestCase.java
+++ src/test/java/org/jboss/msc/value/InjectedSetValueTestCase.java
@@ -1,8 +1,8 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat, Inc., and individual contributors
+ * Copyright 2021, Red Hat, Inc., and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full Seting of individual contributors.
+ * distribution for a full listing of individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
